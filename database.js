75002c8cc39a00d1aba23c2eeb6f43d4c5e63e91
IamIronman2023 <kramsotnas@yahoo.com>
2023-03-31 21:13:38 +0800

    Add mysql2, database.js, use mysql2

---
+++ database.js
@@ -0,0 +1,10 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createPool({
+    host: process.env.HOST,
+    database: process.env.DB,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+  }).promise();  
+
+module.exports = connection;
--- index.js
+++ index.js
@@ -2,20 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
-const mysql = require("mysql");
+const db = require("./database");
+
 const logger = require("./middlewares/logger");
-const auth = require("./middlewares/auth");
+// const auth = require("./middlewares/auth");
 
 const PORT = process.env.PORT || 5000;
-
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  database: process.env.DB,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-});
-
-connection.connect();
 
 //middle ware separate topic
 app.use(express.static(path.join(__dirname, "public")));
@@ -29,42 +21,31 @@ app.listen(PORT, () => {
 });
 
 // Get Request
-app.get("/api/users", auth, (req, res) => {
-  console.log("Get Users ");
-  console.log(`Authenticated ${req.authenticated}`);
-
-  connection.query("SELECT * FROM users", (err, rows, fields) => {
-    if (err) {
-      throw err;
-    } else {
-      res.json(rows);
-    }
-  });
+app.get("/api/users", async (req, res) => { 
+  try {
+    const [rows] = await db.query("SELECT * FROM users"); 
+    res.json(rows);      
+  } catch (error) {
+    throw error;
+  }
 });
 
 // Get Request Parameterized
-app.get("/api/user/:id", (req, res) => {
+app.get("/api/users/:id", async (req, res) => {
   let id = req.params.id;
 
-  //SQL Injection
-  // connection.query(
-  //   `SELECT * FROM employees WHERE id = ${id}`,
+  try {
+    const [rows] = await db.query("SELECT * FROM users WHERE id = ?", [id]);
+    if (rows.length > 0) {
+      res.json(rows);
+    } else {
+      res.status(400).json({ msg: `id ${id} does not exists` });
+    }      
+  } catch (error) {
+    throw error;
+  }
 
-  connection.query(
-    "SELECT * FROM users WHERE id = ?",
-    [id],
-    (err, rows, fields) => {
-      if (err) {
-        throw err;
-      }
 
-      if (rows.length > 0) {
-        res.json(rows);
-      } else {
-        res.status(400).json({ msg: `id ${id} does not exists` });
-      }
-    }
-  );
 });
 
 // Post Request
@@ -73,7 +54,7 @@ app.post("/api/users", (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
-  connection.query(
+  db.query(
     "INSERT INTO users (name, email, password) VALUES(?, ?, ?)",
     [name, email, password],
     (err, result, fields) => {
@@ -90,7 +71,7 @@ app.post("/api/users", (req, res) => {
 app.put("/api/users", (req, res) =>{
   const { id, name, email, password } = req.body;
 
-  connection.query("UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?", 
+  db.query("UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?", 
   [name, email, password, id],
   (err, result, fields) => {
     if (err) {
@@ -105,7 +86,7 @@ app.put("/api/users", (req, res) =>{
 app.delete("/api/users", (req, res) =>{
   const { id } = req.body;
 
-  connection.query("DELETE FROM users WHERE id = ?", 
+  db.query("DELETE FROM users WHERE id = ?", 
   [id],
   (err, result, fields) => {
     if (err) {
